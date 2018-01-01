877cc397da98dcbe1e5ba8a822e2c76bf637f5a2
JamesCraster <jamescraster99@gmail.com>
2018-01-01 21:18:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,19 @@
     OpenWerewolf, an online one-night mafia game.
     Copyright (C) 2017 James Vaughan Craster  
 
-    This program is free software: you can redistribute it and/or modify
+    This file is part of OpenWerewolf. 
+     
+    OpenWerewolf is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
     by the Free Software Foundation, version 3 of the License.
 
-    This program is distributed in the hope that it will be useful,
+    OpenWerewolf is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Affero General Public License for more details.
 
     You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    along with OpenWerewolf.  If not, see <http://www.gnu.org/licenses/>.
 */
 "use strict";
 var express = require('express');
