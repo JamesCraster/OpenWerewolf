cab719689f4c7521563db3d0bd1b5c41a5e5f4c6
JamesCraster <jamescraster99@gmail.com>
2018-03-01 13:21:02 +0000

    added game class for multiple concurrent games

--- index.js
+++ index.js
@@ -31,7 +31,10 @@ class Player{
   constructor(socket){
     this._socket = socket;
     this._registered = false;
+    this._inGame = false;
     this._username = "randomuser";
+    this.data = new Object;
+    this.game;
   }
   get id(){
     return this._socket.id;
@@ -60,7 +63,32 @@ class Server{
     this._registeredPlayerCount = 0;
     this._minPlayerCount = 5;
     this.game = new MessageRoom();
-  };
+    this._games = [];
+    this._games.push(new Game());
+    this._games.push(new Game());
+    setInterval(this.joinGame.bind(this),500);
+  }
+  joinGame(){
+    for(var i = 0; i < this._players.length; i++){
+     //if player is waiting to join a game
+     if(this._players[i].registered && !this._players[i]._inGame){
+      for(var j = 0; j < this._games.length; j++){
+        //if game needs a player
+        if(this._games[j].playersNeeded > 0){
+         this._games[j].addPlayer(this._players[i]);
+         this._players[i]._inGame = true;
+         this._players[i].game = j;
+         this._players[i].send("Hi, " + this._players[i].username + "! You have joined Game " + (j+1).toString() + "."); 
+         break;
+        }
+      }
+      //otherwise (there must be a better way, instead of spamming the chat full!)
+      if(this._players[i]._inGame == false){
+          this._players[i].send("All Games are currently full. Games only last 5 minutes, so there should be one available very soon!");
+      }
+     }
+    }
+  }
   static cleanUpUsername(username){
     username = username.toLowerCase();
     username = username.replace(/\s/g,'');
@@ -104,22 +132,27 @@ class Server{
       player.register();
       player.setUsername(msg);
       this._registeredPlayerCount++;
-      if(this.playersNeeded > 0){
-        Server.broadcast(player.username + ' has joined the game. Game will begin when '+
-         this.playersNeeded.toString() + " more players have joined." , "bold","green"); 	
-      }else{
-        Server.broadcast(player.username + ' has joined the game. The game begins now.', "bold","green");
-      }
+      //if(this.playersNeeded > 0){
+       // Server.broadcast(player.username + ' has joined the game. Game will begin when '+
+       //  this.playersNeeded.toString() + " more players have joined." , "bold","green"); 	
+      //}else{
+       // Server.broadcast(player.username + ' has joined the game. The game begins now.', "bold","green");
+     // }
+    //}
     }
   }
   receive(id,msg){
+    //account for undefined case
     var player = this.getPlayer(id);
     if(!player.registered){
       this.register(player,msg);
     }else{
       if(msg.trim() != ""){
-        Server.broadcast(player.username + ': ' + msg, "black");
+        this._games[player.game].broadcast(player.username + ': ' + msg, "black");
       }
+      //if(msg.trim() != ""){
+       // Server.broadcast(player.username + ': ' + msg, "black");
+      //}
     }
   }
   getPlayer(id){
@@ -133,6 +166,7 @@ class Server{
   kick(id){
     var player = this.getPlayer(id);
     var index = this._players.indexOf(player);
+    //should be index != undefined
     if (index !== -1) {
         this._players.splice(index, 1);
         if(player.registered && this._registeredPlayerCount > 0){
@@ -143,20 +177,97 @@ class Server{
     }
   }
 }
-
-class MessageRoom{
+class Game{
   constructor(){
     this._players = [];
+    this._registeredPlayerCount = 0;
+    this._minPlayerCount = 2;
+    this.messageRoom = new MessageRoom();
   }
-  getPlayer(id){
-    for(var i = 0; i < this._players.length; i++){
-      if(this._players[i].id == id){
-        return this._players[i];
+  get playersNeeded(){
+    return this._minPlayerCount - this._registeredPlayerCount;
+  }
+  addPlayer(player){
+    this._players.push(player);   
+    this._registeredPlayerCount++;
+  }
+  broadcast(msg){
+      for(var i = 0; i < this._players.length; i++){
+        this._players[i].send(msg);
       }
-    }
+      
   }
 }
+class MessageRoomMember{
+    constructor(player){
+        this._player = player;
+        this._muted = false;
+        this._deafened = false;
+    };
+    get muted(){
+     return this._muted;   
+    }
+    get deafened(){
+     return this._deafened;   
+    }
+    get player(){
+     return this._player;   
+    }
+    
+}
+class MessageRoom{
+  constructor(){
+    this._members = [];
+  }
+  getMemberById(id){
+    for(var i = 0; i < this._members.length; i++){
+      if(this._members[i].id == id){
+        return this._members[i];
+      }
+    }
+  } 
+  //make this accept only a msg with no sender in addition
+  broadcast(sender,msg){
+    if(!sender.muted)
+        for(var i = 0; i < this._members.length; i++){
+          if(!this._members[i].deafened){
+              this._members[i].player.send(msg);
+          }
+        }
+  }
+  addPlayer(){
+      
+  }
+  mute(id){
+      member = this.getMemberById(id);
+  }
+  deafen(id){
+      member = this.getMemberById(id);
+  }
+  unmute(id){
+      member = this.getMemberById(id);
+  }
+  undeafen(id){
+      member = this.getMemberById(id);
+  }
+  muteAll(){
+      
+  }
+  deafenAll(){
+      
+  }
+  unmuteAll(){
+      
+  }
+  deafenAll(){
+      
+  }
+  
+}
+
 var server = new Server();
+
+
 io.on('connection', function(socket){
   server.addPlayer(new Player(socket));
   socket.on('message', function(msg){
