623c6290a4f00e524464677ca4df3aa97ca3b80c
JamesCraster <jamescraster99@gmail.com>
2018-03-17 13:26:12 +0000

    Revert "Delete client.js"
    This reverts commit 01fe3dbfd3baeef838850fd697b2491afda3921c.

---
+++ Client/client.js
@@ -0,0 +1,32 @@
+/*   Copyright (C) 2017 James Vaughan Craster. This file is part of OpenWerewolf. 
+     OpenWerewolf is free software: you can redistribute it and/or modify
+     it under the terms of the GNU Affero General Public License as published
+     by the Free Software Foundation, version 3 of the License.
+     OpenWerewolf is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU Affero General Public License for more details.
+     You should have received a copy of the GNU Affero General Public License
+     along with OpenWerewolf.  If not, see <http://www.gnu.org/licenses/>*/
+
+$(function () {
+    var socket = io();
+    
+    $('form').submit(function(){
+      if($('#msg').val() == ""){
+        return false;
+      }
+      socket.emit('message', $('#msg').val());
+      $('#msg').val('');
+      //$('#msg')[0].placeholder = "";
+      return false;
+    });
+    
+    socket.on('message', function(msg){
+      $('#chatbox').append($('<li>').text(msg));
+      $('#inner')[0].scrollTop = $('#inner')[0].scrollHeight;
+    });
+
+    socket.on('registered',function(){
+    })
+});
